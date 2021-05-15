8d80cb7032cee133d783b625aa910931c83595d8
kedmonmaquiz <jkedmon95@gmail.com>
2021-05-15 03:40:31 +0300

    create relationship between product and reviews

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Review;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +10,8 @@ class Product extends Model
 {
     use HasFactory;
 
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
     use HasFactory;
+
+    public function product()
+    {
+        return $this->bolongsTo(Product::class);
+    }
 }
